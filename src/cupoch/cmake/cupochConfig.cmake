83d22a4750510f1c50242975af35ca00f9085f95
Soroush Rabiei <soroush.rabiei@gmail.com>
2021-03-19 21:36:57 +0100

    Remove IO module also from the buildsystem
    Signed-off-by: Soroush Rabiei <soroush.rabiei@gmail.com>

--- src/cupoch/cmake/cupochConfig.cmake
+++ src/cupoch/cmake/cupochConfig.cmake
@@ -7,5 +7,5 @@ find_package(cupoch_integration CONFIG REQUIRED PATHS "${CMAKE_CURRENT_LIST_DIR}
 find_package(cupoch_registration CONFIG REQUIRED PATHS "${CMAKE_CURRENT_LIST_DIR}")
 find_package(cupoch_odometry CONFIG REQUIRED PATHS "${CMAKE_CURRENT_LIST_DIR}")
 find_package(cupoch_planning CONFIG REQUIRED PATHS "${CMAKE_CURRENT_LIST_DIR}")
-find_package(cupoch_io CONFIG REQUIRED PATHS "${CMAKE_CURRENT_LIST_DIR}")
+# find_package(cupoch_io CONFIG REQUIRED PATHS "${CMAKE_CURRENT_LIST_DIR}")
 include("${CMAKE_CURRENT_LIST_DIR}/cupochTargets.cmake")
