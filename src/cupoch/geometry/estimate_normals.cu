c9f32d1cf4236bb4c48ead358b25cc88f3e56c16
nekanat <nekanat.stock@gmail.com>
2021-06-07 11:53:28 +0900

    devide eigenvalue

--- src/cupoch/geometry/estimate_normals.cu
+++ src/cupoch/geometry/estimate_normals.cu
@@ -25,158 +25,13 @@
 #include "cupoch/geometry/kdtree_flann.h"
 #include "cupoch/geometry/pointcloud.h"
 #include "cupoch/utility/console.h"
+#include "cupoch/utility/eigenvalue.h"
 #include "cupoch/utility/range.h"
 
 using namespace cupoch;
 using namespace cupoch::geometry;
 
 namespace {
-
-__device__ float signf(float x) { return x / fabs(x); }
-
-__device__ Eigen::Vector3f ComputeEigenvector0(const Eigen::Matrix3f &A,
-                                               float eval0) {
-    Eigen::Vector3f row0(A(0, 0) - eval0, A(0, 1), A(0, 2));
-    Eigen::Vector3f row1(A(0, 1), A(1, 1) - eval0, A(1, 2));
-    Eigen::Vector3f row2(A(0, 2), A(1, 2), A(2, 2) - eval0);
-    Eigen::Vector3f rxr[3];
-    rxr[0] = row0.cross(row1);
-    rxr[1] = row0.cross(row2);
-    rxr[2] = row1.cross(row2);
-    Eigen::Vector3f d;
-    d[0] = rxr[0].dot(rxr[0]);
-    d[1] = rxr[1].dot(rxr[1]);
-    d[2] = rxr[2].dot(rxr[2]);
-
-    int imax;
-    d.maxCoeff(&imax);
-    return rxr[imax] / sqrtf(d[imax]);
-}
-
-__device__ Eigen::Vector3f ComputeEigenvector1(const Eigen::Matrix3f &A,
-                                               const Eigen::Vector3f &evec0,
-                                               float eval1) {
-    float max_evec0_abs = max(fabs(evec0(0)), fabs(evec0(1)));
-    float inv_length =
-            1 / sqrtf(max_evec0_abs * max_evec0_abs + evec0(2) * evec0(2));
-    Eigen::Vector3f U = (fabs(evec0(0)) > fabs(evec0(1)))
-                                ? Eigen::Vector3f(-evec0(2), 0, evec0(0))
-                                : Eigen::Vector3f(0, evec0(2), -evec0(1));
-    U *= inv_length;
-    Eigen::Vector3f V = evec0.cross(U);
-
-    Eigen::Vector3f AU(A(0, 0) * U(0) + A(0, 1) * U(1) + A(0, 2) * U(2),
-                       A(0, 1) * U(0) + A(1, 1) * U(1) + A(1, 2) * U(2),
-                       A(0, 2) * U(0) + A(1, 2) * U(1) + A(2, 2) * U(2));
-
-    Eigen::Vector3f AV = {A(0, 0) * V(0) + A(0, 1) * V(1) + A(0, 2) * V(2),
-                          A(0, 1) * V(0) + A(1, 1) * V(1) + A(1, 2) * V(2),
-                          A(0, 2) * V(0) + A(1, 2) * V(1) + A(2, 2) * V(2)};
-
-    float m00 = U(0) * AU(0) + U(1) * AU(1) + U(2) * AU(2) - eval1;
-    float m01 = U(0) * AV(0) + U(1) * AV(1) + U(2) * AV(2);
-    float m11 = V(0) * AV(0) + V(1) * AV(1) + V(2) * AV(2) - eval1;
-
-    float absM00 = fabs(m00);
-    float absM01 = fabs(m01);
-    float absM11 = fabs(m11);
-    float max_abs_comp0 = max(absM00, absM11);
-    float max_abs_comp = max(max_abs_comp0, absM01);
-    float coef2 = min(max_abs_comp0, absM01) / max(max_abs_comp, 1.0e-6);
-    float coef1 = 1.0 / sqrtf(1.0 + coef2 * coef2);
-    if (absM00 >= absM11) {
-        coef2 *= coef1 * signf(m00) * signf(m01);
-        return (max_abs_comp0 >= absM01) ? coef2 * U - coef1 * V
-                                         : coef1 * U - coef2 * V;
-    } else {
-        coef2 *= coef1 * signf(m11) * signf(m01);
-        return (max_abs_comp0 >= absM01) ? coef1 * U - coef2 * V
-                                         : coef2 * U - coef1 * V;
-    }
-}
-
-__device__ Eigen::Vector3f FastEigen3x3(Eigen::Matrix3f &A) {
-    // Previous version based on:
-    // https://en.wikipedia.org/wiki/Eigenvalue_algorithm#3.C3.973_matrices
-    // Current version based on
-    // https://www.geometrictools.com/Documentation/RobustEigenSymmetric3x3.pdf
-    // which handles edge cases like points on a plane
-
-    float max_coeff = A.maxCoeff();
-    if (max_coeff == 0) {
-        return Eigen::Vector3f::Zero();
-    }
-    A /= max_coeff;
-
-    float norm = A(0, 1) * A(0, 1) + A(0, 2) * A(0, 2) + A(1, 2) * A(1, 2);
-    if (norm > 0) {
-        Eigen::Vector3f eval;
-        Eigen::Vector3f evec0;
-        Eigen::Vector3f evec1;
-        Eigen::Vector3f evec2;
-
-        float q = (A(0, 0) + A(1, 1) + A(2, 2)) / 3;
-
-        float b00 = A(0, 0) - q;
-        float b11 = A(1, 1) - q;
-        float b22 = A(2, 2) - q;
-
-        float p = sqrtf((b00 * b00 + b11 * b11 + b22 * b22 + norm * 2) / 6);
-
-        float c00 = b11 * b22 - A(1, 2) * A(1, 2);
-        float c01 = A(0, 1) * b22 - A(1, 2) * A(0, 2);
-        float c02 = A(0, 1) * A(1, 2) - b11 * A(0, 2);
-        float det = (b00 * c00 - A(0, 1) * c01 + A(0, 2) * c02) / (p * p * p);
-
-        float half_det = det * 0.5;
-        half_det = min(max(half_det, -1.0), 1.0);
-
-        float angle = acos(half_det) / (float)3;
-        float const two_thirds_pi = 2.09439510239319549;
-        float beta2 = cos(angle) * 2;
-        float beta0 = cos(angle + two_thirds_pi) * 2;
-        float beta1 = -(beta0 + beta2);
-
-        eval(0) = q + p * beta0;
-        eval(1) = q + p * beta1;
-        eval(2) = q + p * beta2;
-
-        if (half_det >= 0) {
-            evec2 = ComputeEigenvector0(A, eval(2));
-            if (eval(2) < eval(0) && eval(2) < eval(1)) {
-                A *= max_coeff;
-                return evec2;
-            }
-            evec1 = ComputeEigenvector1(A, evec2, eval(1));
-            A *= max_coeff;
-            if (eval(1) < eval(0) && eval(1) < eval(2)) {
-                return evec1;
-            }
-            evec0 = evec1.cross(evec2);
-            return evec0;
-        } else {
-            evec0 = ComputeEigenvector0(A, eval(0));
-            if (eval(0) < eval(1) && eval(0) < eval(2)) {
-                A *= max_coeff;
-                return evec0;
-            }
-            evec1 = ComputeEigenvector1(A, evec0, eval(1));
-            A *= max_coeff;
-            if (eval(1) < eval(0) && eval(1) < eval(2)) {
-                return evec1;
-            }
-            evec2 = evec0.cross(evec1);
-            return evec2;
-        }
-    } else {
-        A *= max_coeff;
-        int min_id;
-        A.diagonal().minCoeff(&min_id);
-        Eigen::Vector3f unit = Eigen::Vector3f::Zero();
-        unit[min_id] = 1.0;
-        return unit;
-    }
-}
 
 __device__ Eigen::Vector3f ComputeNormal(const Eigen::Matrix<float, 9, 1> &cum,
                                          int count) {
@@ -193,7 +48,7 @@ __device__ Eigen::Vector3f ComputeNormal(const Eigen::Matrix<float, 9, 1> &cum,
     covariance(1, 2) = cumulants(7) - cumulants(1) * cumulants(2);
     covariance(2, 1) = covariance(1, 2);
 
-    return FastEigen3x3(covariance);
+    return utility::FastEigen3x3(covariance);
 }
 
 struct compute_normal_functor {
