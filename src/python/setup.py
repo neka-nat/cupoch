a077bfd2cbf1a54418a0415a6fce9a49e191fb47
daglof <dag.lofthus@solwr.com>
2026-09-10 10:52:33 +0200

    Necessary changes to support python 3.13 and 3.14

--- src/python/setup.py
+++ src/python/setup.py
@@ -55,6 +55,8 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Topic :: Education",
         "Topic :: Multimedia :: Graphics :: 3D Rendering",
         "Topic :: Multimedia :: Graphics :: Viewers",
